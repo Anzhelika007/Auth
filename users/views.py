638a5ae97596b0520b20a7041dfb321b8c7aafb9
Anzhalika Turlak <turlak.by@gmail.com>
2022-10-08 14:45:36 +0300

    add jwt token

--- users/views.py
+++ users/views.py
@@ -1,9 +1,15 @@
 import email
 from http import server
+import secrets
 from django.shortcuts import render
 from rest_framework.views import APIView
 from .serializers import UserSerializer
 from rest_framework.response import Response
+from .models import User
+from rest_framework.exceptions import AuthenticationFailed
+import jwt, datetime  
+
+
 
 class RegisterView(APIView):
     def post(self, request):
@@ -16,3 +22,20 @@ class LoginView(APIView):
     def post(self, request):
         email = request.data['email']
         password = request.data['password']
+
+        user = User.objects.filter(email = email).first()
+
+        if user is None:
+            raise AuthenticationFailed('Пользователь не найден')
+
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=300),
+            'iat': datetime.datetime.utcnow()
+        }
+
+
+        token = jwt.encode(payload, 'secret', algorithm='HS256').decode('utf-8')
+
+
+        return Response({'jwt': token})
